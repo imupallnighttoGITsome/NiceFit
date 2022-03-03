db6e97bfc3a79af5933fe7950f6e9ef36353bf12
imupallnighttogitsome <artishard.palmer@gmail.com>
2022-03-03 16:44:54 -0600

    np

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const dotenv = require("dotenv");
 
 const app = express();
+dotenv.config();
 
 
 app.get("/", (req, res) => {
@@ -8,4 +10,7 @@ app.get("/", (req, res) => {
 });
 
 
-app.listen(5000, console.log('server started on port 5000'))
+const PORT = process.env.PORT || 5000;
+
+
+app.listen(PORT, console.log(`server started on ${PORT}`))
