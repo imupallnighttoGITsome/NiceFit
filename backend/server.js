00db56b0f1494a392302ea6b566d21e1d8f4dfc6
imupallnighttogitsome <artishard.palmer@gmail.com>
2022-03-09 15:26:40 -0600

    working

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const path = require("path");
 // const dotenv = require("dotenv");
 // const outfits = require("./seeddata.js");
 
-const apiRouter = require("./controller/api");
+const apiRouter = require("./controller");
 
 const port = process.env.PORT || 4000;
 const isProduction = process.env.NODE_ENV === "production";
@@ -16,13 +16,15 @@ app.use(express.urlencoded.apply({ extended: true }));
 app.use(express.static(path.join(__dirname, "../build")));
 // dotenv.config();
 
-app.get("/", (req, res) => {
-  res.send("API is runnninin");
-});
 
-app.get("/outfits", (req, res) => {
-  res.send(outfits);
-});
+app.use('/nicefit/', apiRouter)
+// app.get("/", (req, res) => {
+//   res.send("API is runnninin");
+// });
+
+// app.get("/outfits", (req, res) => {
+//   res.send(outfits);
+// });
 
 app.listen(port, () => {
   console.log(`port ${port} | production: ${isProduction}`);
