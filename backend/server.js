ac8a1af9bcee6ff189ef8645ea46e3547911844c
imupallnighttogitsome <artishard.palmer@gmail.com>
2022-03-14 16:48:15 -0500

    hmm

--- backend/server.js
+++ backend/server.js
@@ -12,15 +12,18 @@ const isProduction = process.env.NODE_ENV === "production";
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded.apply({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "../build")));
 // dotenv.config();
 
 
 app.use('/nicefit/', apiRouter)
-// app.get("/", (req, res) => {
-//   res.send("API is runnninin");
-// });
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../build"), (err) => {
+    console.log('problem sendfile of react-build')
+  });
+});
 
 // app.get("/outfits", (req, res) => {
 //   res.send(outfits);
