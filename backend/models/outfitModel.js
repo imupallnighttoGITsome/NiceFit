f111ae45f790934cb064a8a96161d7904a09551a
imupallnighttogitsome <artishard.palmer@gmail.com>
2022-03-25 16:58:54 -0500

    adding outfitscehma model

---
+++ backend/models/outfitModel.js
@@ -0,0 +1,6 @@
+const mongoose =  require('mongoose');
+
+const outFitSchema =  mongoose.Schema({
+    //hat, glasses, top, bottom, socks, shoes/// all type string //// required: [true, 'Please Add Description']
+
+    })
