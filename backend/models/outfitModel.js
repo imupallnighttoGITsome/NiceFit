5e66d66e129bea4aedc54f196fded32a96f48c14
imupallnighttogitsome <artishard.palmer@gmail.com>
2022-03-26 12:12:43 -0500

    working on mongo compass

--- backend/models/outfitModel.js
+++ backend/models/outfitModel.js
@@ -1,38 +1,41 @@
-const mongoose =  require('mongoose');
+const mongoose = require("mongoose");
 
-const outFitSchema =  mongoose.Schema({
+const outFitSchema = mongoose.Schema(
+  {
     //hat, glasses, top, bottom, socks, shoes/// all type string //// required: [true, 'Please Add Description']
     hat: {
-        type: String,
-        required: [true, 'Please Add a Description']
+      type: String,
+      required: [true, "Please Add a Description"],
     },
 
     glasses: {
-        type: String,
-        required: [true, 'Please Add a Description']
+      type: String,
+      required: [true, "Please Add a Description"],
     },
 
     top: {
-        type: String,
-        required: [true, 'Please Add a Description']
+      type: String,
+      required: [true, "Please Add a Description"],
     },
 
     bottoms: {
-        type: String,
-        required: [true, 'Please Add a Description']
+      type: String,
+      required: [true, "Please Add a Description"],
     },
 
     socks: {
-        type: String,
-        required: [true, 'Please Add a Description']
+      type: String,
+      required: [true, "Please Add a Description"],
     },
 
     shoes: {
-        type: String,
-        required: [true, 'Please Add a Description']
+      type: String,
+      required: [true, "Please Add a Description"],
     },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-
-});
-
-module.exports = mongoose.model('Outfit', outFitSchema)
+module.exports = mongoose.model("Outfit", outFitSchema);
