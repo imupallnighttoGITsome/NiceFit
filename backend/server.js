0f2e2748ff614d206cef7b05d5f0921c72e7fb53
imupallnighttogitsome <artishard.palmer@gmail.com>
2022-07-01 14:57:23 -0500

    initial REST API for outfits

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,8 @@ const dotenv = require('dotenv').config();
 const { errorHandler } =  require('./middleware/errorMiddleware');
 const connectDB = require('./config/conn')
 const port = process.env.PORT || 5000;
-const cors = require('cors');
-const path = require('path');
+// const cors = require('cors');
+// const path = require('path');
 // const outfits = require("./seeddata.js");
 
 // const apiRouter = require('./controllers/controllers');
