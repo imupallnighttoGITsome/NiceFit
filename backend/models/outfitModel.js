3227e818c0b0b93703541bcb88da685711dad7d3
imupallnighttogitsome <artishard.palmer@gmail.com>
2022-07-01 16:59:18 -0500

    working on user authentication

--- backend/models/outfitModel.js
+++ backend/models/outfitModel.js
@@ -4,7 +4,7 @@ const outFitSchema = mongoose.Schema(
   {
     user: {
       type: mongoose.Schema.Types.ObjectId,
-      // required: true,
+      required: true,
       ref: "User",
     },
     hat: String,
