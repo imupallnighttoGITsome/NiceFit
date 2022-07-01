047e2278200c59bce25ea7c6c91a77ea805dca52
imupallnighttogitsome <artishard.palmer@gmail.com>
2022-07-01 13:58:41 -0500

    nicefit controllers and routes setup

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,8 @@ const dotenv = require('dotenv').config();
 const { errorHandler } =  require('./middleware/errorMiddleware');
 const connectDB = require('./config/conn')
 const port = process.env.PORT || 5000;
-// const cors = require('cors');
-// const path = require('path');
+const cors = require('cors');
+const path = require('path');
 // const outfits = require("./seeddata.js");
 
 // const apiRouter = require('./controllers/controllers');
@@ -15,7 +15,7 @@ connectDB();
 const app = express();
 // app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 // app.use(express.static(path.join(__dirname, '../build')));
 // dotenv.config();
 
